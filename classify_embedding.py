cc1318f216abebadb8359f410966b5c7c7a0c777
cuichenx <bob.cui@mail.utoronto.ca>
2021-11-12 14:32:01 -0500

    add scripts for peripheral experiments

---
+++ classify_embedding.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+from utils import regex_parsable, hmong_syllable_component
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+run_name = 'grpd2_letters_chardim8_Clf_run1'
+print('run_name is', run_name)
+model_path = f"model/{run_name}/best_model"
+embeds = torch.load(model_path, map_location='cpu')['embeds.weight']
+
+
+data_path = "data/data.pth"
+data = torch.load(data_path)
+i2w = data['i2w']
+
+valid_wi = []
+y_tones = []  # ton labels
+tones = '_bdgjmsv'
+tone2i = {'': 0, 'b': 1, 'd': 2, 'g': 3, 'j': 4, 'm': 5, 's': 6, 'v': 7}
+i2tone = ['', 'b', 'd', 'g', 'j', 'm', 's', 'v']
+
+for i, w in enumerate(i2w):
+    if regex_parsable(w):
+        valid_wi.append(i)
+        y_tones.append(hmong_syllable_component(w, 'ton'))
+
+X = embeds[np.array(valid_wi)]
+y = list(map(tone2i.get, y_tones))
+print(len(X), len(y))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# clf = SVC()
+clf = MLPClassifier()
+
+clf.fit(X_train, y_train)
+mean_acc = clf.score(X_test, y_test)
+print('mean accuracy is', mean_acc)
+
+
+
---
+++ confusion_matrix.py
@@ -0,0 +1,29 @@
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
+run_name = "temp_grpd3_nochar_run1"
+print("run_name is", run_name)
+
+
+pred_file_path = f"evaluation/{run_name}/pred.txt"
+
+with open(pred_file_path) as f:
+    txt = f.read().split('\n')
+
+y_test = []
+y_pred = []
+for line in txt:
+    if line:
+        line_split = line.split(' ')
+        if len(line_split) == 3:
+            word, gt, pred = line_split
+            y_pred.append(pred)
+            y_test.append(gt)
+        else:
+            print("this line is weird:", line)
+
+
+ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+plt.title(run_name)
+plt.savefig(f"{run_name}_confusion.png")
